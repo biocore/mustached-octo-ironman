28f28af4a5d2f373b172c46af01988a096ff890b
Daniel McDonald <mcdonadt@colorado.edu>
2014-10-17 12:54:17 -0600

    job_id -> id

--- moi/job.py
+++ moi/job.py
@@ -5,15 +5,15 @@ from functools import partial
 from moi import r_client, ctx, REDIS_KEY_TIMEOUT
 
 
-def _status_change(job_id, redis, new_status):
+def _status_change(id, redis, new_status):
     """Update the status of a job
 
-    The status associated with the job_id is updated, an update command is
+    The status associated with the id is updated, an update command is
     issued to the job's pubsub, and and the old status is returned.
 
     Parameters
     ----------
-    job_id : str
+    id : str
         The job ID
     redis : Redis
         A Redis client
@@ -25,12 +25,12 @@ def _status_change(job_id, redis, new_status):
     str
         The old status
     """
-    job_info = json.loads(redis.get(job_id))
+    job_info = json.loads(redis.get(id))
     old_status = job_info['status']
     job_info['status'] = new_status
 
     with redis.pipeline() as pipe:
-        pipe.set(job_id, json.dumps(job_info), ex=REDIS_KEY_TIMEOUT)
+        pipe.set(id, json.dumps(job_info), ex=REDIS_KEY_TIMEOUT)
         pipe.publish(job_info['pubsub'], json.dumps({"update": "status"}))
         pipe.execute()
 
@@ -50,7 +50,7 @@ def _redis_wrap(redis_deets, func, *args, **kwargs):
     ----------
     redis_deets : dict
         Redis details, specifically:
-            {'job_id': <str, the job ID>,
+            {'id': <str, the job ID>,
              'pubsub': <str, the pubsub key for the job>,
              'group': <str, the group the job is a part of>,
              'handler': <URL or None, the results handler>}
@@ -68,22 +68,22 @@ def _redis_wrap(redis_deets, func, *args, **kwargs):
 
         """
         pubsub = job_info['pubsub']
-        job_id = job_info['job_id']
+        id = job_info['id']
         job_data_serialized = json.dumps(job_info)
 
         with r_client.pipeline() as pipe:
-            pipe.set(job_id, job_data_serialized, ex=REDIS_KEY_TIMEOUT)
+            pipe.set(id, job_data_serialized, ex=REDIS_KEY_TIMEOUT)
             pipe.publish(pubsub, json.dumps({"update": None}))
             pipe.execute()
 
-    job_info = {'job_id': redis_deets['job_id'],
+    job_info = {'id': redis_deets['id'],
                 'pubsub': redis_deets['pubsub'],
                 'handler': redis_deets['handler'],
                 'group': redis_deets['group'],
                 'status': 'Running',
                 'result': None}
 
-    status_changer = partial(_status_change, job_info['job_id'], r_client)
+    status_changer = partial(_status_change, job_info['id'], r_client)
     kwargs['update_status'] = status_changer
 
     _deposit_payload(job_info)
@@ -123,23 +123,44 @@ def submit(group, name, handler, func, *args, **kwargs):
     str
         The job ID
     """
-    job_id = str(uuid4())
+    id = str(uuid4())
 
     group_jobs_key = group + ':jobs'
     group_pubsub_key = group + ':pubsub'
 
-    job_pubsub_key = job_id + ':pubsub'
+    job_pubsub_key = id + ':pubsub'
 
     with r_client.pipeline() as pipe:
         # add the job to the group
-        pipe.sadd(group_jobs_key, job_id)
-        pipe.publish(group_pubsub_key, {'add': job_id})
+        pipe.sadd(group_jobs_key, id)
+        pipe.publish(group_pubsub_key, {'add': id})
         pipe.execute()
 
     redis_deets = {'pubsub': job_pubsub_key,
-                   'job_id': job_id,
+                   'id': id,
                    'group': group,
                    'handler': handler}
 
-    ar = ctx.submit_async(_redis_wrap, redis_deets, func, *args, **kwargs)
-    return job_id, ar
+    ctx.submit_async(_redis_wrap, redis_deets, func, *args, **kwargs)
+    return id
+
+
+def submit_nouser(func, *args, **kwargs):
+    """Submit a function to a cluster without an associated user
+
+    Parameters
+    ----------
+    func : function
+        The function to execute. Any returns from this function will be
+        serialized and deposited into Redis using the uuid for a key.
+    args : tuple or None
+        Any args for ``f``
+    kwargs : dict or None
+        Any kwargs for ``f``
+
+    Returns
+    -------
+    str
+        The job ID
+    """
+    return submit("no-user", "unnamed", None, func, *args, **kwargs)
