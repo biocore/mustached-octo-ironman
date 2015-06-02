336abcb1f2628f0f9103f0202e2802e808b99318
Daniel McDonald <mcdonadt@colorado.edu>
2015-06-01 20:59:47 -0600

    stillnotthere

--- script_test.sh
+++ script_test.sh
@@ -1,7 +1,7 @@
-moi submit --user=ci-test-py --cmd="return 42"
-moi submit --user=ci-test-py --cmd="float('crash and burn')"
-moi submit --user=ci-test-sys --cmd="echo 42" --cmd-type=system
-moi submit --user=ci-test-sys --cmd="crash-and-burn" --cmd-type=system
+./scripts/moi submit --user=ci-test-py --cmd="return 42"
+./scripts/moi submit --user=ci-test-py --cmd="float('crash and burn')"
+./scripts/moi submit --user=ci-test-sys --cmd="echo 42" --cmd-type=system
+./scripts/moi submit --user=ci-test-sys --cmd="crash-and-burn" --cmd-type=system
 
 py_fail_count=$(moi userjobs --key=ci-test-py --summary | grep -c Failed)
 py_win_count=$(moi userjobs --key=ci-test-py --summary | grep -c Success)
