65bd91a4e15e5de9f133a651636f448eaa3e908d
Daniel McDonald <mcdonadt@colorado.edu>
2015-06-04 13:02:49 -0600

    Bump version number to 0.2.0

--- moi/__init__.py
+++ moi/__init__.py
@@ -75,6 +75,6 @@ if failed:
 ctx_default = _config.get('ipython', 'default')
 
 
-__version__ = '0.1.0-dev'
+__version__ = '0.2.0'
 __all__ = ['r_client', 'ctxs', 'ctx_default', 'REDIS_KEY_TIMEOUT', 'moi_js',
            'moi_list_js']
